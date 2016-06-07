fe82a176a0a375d4083d5173f668e11edb131ed7
Todor Krastev <to6esko69@gmail.com>
2016-06-07 11:44:06 +0300

    add create task

--- src/components/app.js
+++ src/components/app.js
@@ -18,12 +18,23 @@ export default class App extends React.Component{
         super(props);
         this.state = {todos};
     }
+
+    createTask(task) {
+        this.state.todos.push({
+            task,
+            isCompleted: false
+        });
+        this.setState({ todos: this.state.todos });
+    }
+
+    
     render() {
         return (
             <div>
                 <h1>React todo list</h1>
-                <CreateTodo />
-                <TodosList todos={this.state.todos}/>
+                <CreateTodo createTask={this.createTask.bind(this)}/>
+                <TodosList
+                    todos={this.state.todos}/>
             </div>
         );
     }
--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 
 export default class CreateTodo extends React.Component {
-   
+    handleCreate(e) {
+        e.preventDefault();
+         this.props.createTask(this.refs.createInput.value);
+        
+   }
     render() {
         return (
-            <form>
-                <input type="text" placeholder="What do I need to do?"/>
+            <form onSubmit={this.handleCreate.bind(this)}>
+                <input type="text" ref="createInput"placeholder="What do I need to do?"/>
                 <button>Create</button>
                 </form>
         );
