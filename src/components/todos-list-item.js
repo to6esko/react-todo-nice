22b987fb54a6d55aaa9a298735fe7d80120d618e
Todor Krastev <to6esko69@gmail.com>
2016-06-07 11:35:40 +0300

    add button action

--- src/components/todos-list-item.js
+++ src/components/todos-list-item.js
@@ -2,14 +2,41 @@ import React from 'react';
 
 
 export default class TodosListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isEditing: false };
+    }
+
+    onEditClick() {
+        this.setState({ isEditing: true });
+    }
+    onChangeClick() {
+        this.setState({ isEditing: false });
+   }
+
+
+
+    renderActionSection() {
+        if (this.state.isEditing) {
+            return (
+                <td>
+                    <button>Save</button>
+                    <button onClick={this.onChangeClick.bind(this)}>Cansel</button>
+                </td>
+            );
+        }
+        return (
+            <td>
+                <button onClick={this.onEditClick.bind(this) }>Edit</button>
+                <button>Delete</button>
+            </td>
+        );
+    }
     render() {
         return (
             <tr>
                 <td>{this.props.task}</td>
-                <td>
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </td>
+                {this.renderActionSection()}
             </tr>
         );
     }
